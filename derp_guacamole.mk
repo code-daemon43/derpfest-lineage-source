dc4d92fa9048864321b6a1e8d328408603056c27
code-daemon43 <6ix_flame-os@protonmail.com>
2024-09-07 13:05:26 -0400

    Update derp_guacamole.mk

--- derp_guacamole.mk
+++ derp_guacamole.mk
@@ -14,6 +14,15 @@ $(call inherit-product, device/oneplus/guacamole/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Official Tag
+DERP_BUILDTYPE := Official
+
+# Gapps
+TARGET_USES_MINI_GAPPS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_guacamole
 PRODUCT_DEVICE := guacamole
